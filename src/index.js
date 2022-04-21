cef14590882dc62a368726db6256df413487c35e
Vany.Liu <vany8462.fl00@gmail.com>
2022-04-21 17:26:38 +0800

    initial commit

--- src/index.js
+++ src/index.js
@@ -1,17 +1,25 @@
 import React from 'react';
-import ReactDOM from 'react-dom/client';
+import ReactDOM from 'react-dom';
+import * as serviceWorker from './serviceWorker';
 import './index.css';
+// import { Route, BrowserRouter as Router, Switch } from 'react-router-dom';
+import { HashRouter as Router, Routes, Route} from "react-router-dom";
 import App from './App';
-import reportWebVitals from './reportWebVitals';
+import Header from './components/Header';
+import Footer from './components/Footer';
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+const routing = (
+	<Router>
+		<React.StrictMode>
+			<Header />
+			<Routes>
+				<Route path="/" element={<App/>} />
+			</Routes>
+			<Footer />
+		</React.StrictMode>
+	</Router>
 );
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+ReactDOM.render(routing, document.getElementById('root'));
+
+serviceWorker.unregister();
